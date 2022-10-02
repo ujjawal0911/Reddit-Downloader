8b04957bb019c07f279668cfe637b080367b661c
Ujjawal Srivastava <srivastava.ujjawal011@gmail.com>
2022-10-02 23:20:12 +0530

    Added Functionality for Image Download

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from hashlib import new
 import praw
+import os
+import requests
+import zipfile
 from tqdm import tqdm
+from typing import List
+from urllib.parse import urlparse
 
 reddit = praw.Reddit(
     client_id = 'vBOKe5A5jSjLe-i2iCKjww',
@@ -10,14 +14,15 @@ reddit = praw.Reddit(
 
 redditor_username = 'chibibellebear'
 given_redditor = reddit.redditor(redditor_username)
+download_location = os.getcwd()
 
 class RedditorInformation:
     def __init__(self, redditor_username):
         self.redditor = reddit.redditor(redditor_username)
 
 
-    def get_submissions(self, nsfw_filter=False):
-        print('Started Fetching...')
+    def get_submissions_urls(self, nsfw_filter=False) -> List['str']:
+        print('Getting image urls...')
         urls = []
         all_submissions = list(self.redditor.submissions.top(time_filter='all', limit=None))
 		
@@ -28,7 +33,6 @@ class RedditorInformation:
             imgs = ['imgur.com', '.png', '.jpg', '.jpeg']
             if any(img in submission.url for img in imgs):
                 urls.append(submission.url)
-#                print(submission.url)
 
             # If the post is as gallery
             elif 'gallery' in submission.url:
@@ -38,8 +42,17 @@ class RedditorInformation:
                     largest_image = image_item['s']
                     image_url = image_item['s']['u']
                     urls.append(image_url)
-#                    print(submission.url)
-        print(urls)
+        
+        return urls
+
+    def download_all_images(self, submissions: List['reddit.submission'], file_name: str = '{}.zip'.format(redditor_username)):
+        print('Downloading Started...')
+        # Opening the ZipFile and adding files in that
+        with zipfile.ZipFile(file_name, 'w') as img_zip:
+            for image_url in tqdm(submissions, total=len(submissions)):
+                img_name = os.path.basename(urlparse(image_url).path)
+                img_data = requests.get(image_url).content
+                img_zip.writestr(img_name, img_data)
 
     def get_submissions_from_subreddit(self, subreddit):
         pass
@@ -55,9 +68,12 @@ class RedditorInformation:
 
 
 class SubredditInformation:
+    def __init__(self, subreddit: str):
+        self.subreddit = reddit.subreddit(subreddit)
     pass
 
 
 x = RedditorInformation(redditor_username)
-x.get_submissions()
+t = x.get_submissions_urls()
+x.download_all_images(t)
 print(reddit.read_only)
